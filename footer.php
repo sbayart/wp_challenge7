4aee326d1fd6487bdb11955c6283d36add3bb9c6
solene <solenantoine@gmail.com>
2017-08-22 15:19:08 +0200

    header and footer added

--- footer.php
+++ footer.php
@@ -1,2 +1,122 @@
+<footer>
+    <section class="greysection">
+        <h3>Get Live Updates</h3>
+        <p class="sdtitle">A special thanks to Death.</p>
+        <p class="suscribe">Subscribe to our Newsletter</p>
+    </section>
+    <section class="credits">
+        <h3>Credits</h3>
+        <p>A special thanks to Death.</p>
+        <div class="sscredit">
+            <div class="row">
+                <article class="col-xs-6">
+                    <i class="fa fa-desktop fa-4x "></i>
+                    <div class="">
+                        <h4>Bootstrap</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt </p>
+                    </div>
+                </article>
+                <article class="col-xs-6">
+                    <i class="fa fa-picture-o fa-4x "> </i>
+                    <div class="">
+                        <h4>Owl-Carousel</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt </p>
+                    </div>
+                </article>
+            </div>
+            <div class="row">
+                <article class="col-xs-6">
+                    <i class="fa fa-magic fa-4x "> </i>
+                    <div class="">
+                        <h4>Codrops</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt </p>
+                    </div>
+                </article>
+                <article class="col-xs-6">
+                    <i class="fa fa-heart fa-4x "> </i>
+                    <div class="">
+                        <h4>Lorem Ipsum</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt </p>
+                    </div>
+                </article>
+            </div><!--row -->
+        </div>
+    </section>
+    <section class="lady">
+        <div class="">
+            <h3>Download Free</h3>
+            <p class="text-uppercase"><i class="fa fa-usd" aria-hidden="true"></i> Pay with a tweet</p>
+        </div>
+    </section>
+    <section class="contactus">
+        <h3>Contact Us</h3>
+        <p>A special thanks to Death.</p>
+        <div class="form col-xs-6">
+            <div class="form-group has-feedback">
+                <label class="control-label" for="inputSuccess2">Your Name</label>
+                <input type="text" class="form-control" aria-describedby="inputSuccess2Status">
+                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <!-- <span id="inputSuccess2Status" class="sr-only">(success)</span> -->
+            </div>
+            <div class="form-group has-feedback">
+                <label class="control-label" for="inputSuccess2">Your Email</label>
+                <input type="text" class="form-control" aria-describedby="inputSuccess2Status">
+                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <!-- <span id="inputSuccess2Status" class="sr-only">(success)</span> -->
+            </div>
+            <div class="form-group has-feedback">
+                <label class="control-label" for="inputSuccess2">Message</label>
+                <textarea type="text" class="form-control" aria-describedby="inputSuccess2Status"></textarea>
+                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <!-- <span id="inputSuccess2Status" class="sr-only">(success)</span> -->
+            </div>
+            <p class="green">Submit</p>
+        </div>
+        <div class="office col-xs-6">
+            <h4>Office Location</h4>
+            <p>The Pentagon</p>
+            <p>Washington</p>
+            <p>Phone: XXX-XXX-XXXX</p>
+            <p>Fax: XXX-XXX-YYYY</p>
+            <h4 class="social">Social</h4>
+            <i class="fa fa-facebook-square fa-2x"></i>
+            <i class="fa fa-twitter-square fa-2x"></i>
+            <i class="fa fa-google-plus-square fa-2x"></i>
+            <i class="fa fa-flickr fa-2x"></i>
+        </div>
+    </section>
+    <section class="final">
+        <div class="col-xs-12 col-lg-8">
+            <h4>Follow Me!</h4>
+            <p>Vuoi ricevere news su altri template?</p>
+            <p>Visita Andrea Galanti.it e vedrai tutte le news riguardanti nuovi Theme!</p>
+            <p>Go to:andreagalnti.it</p>
+            <p>Flatfy Theme by Andrea Galanti is licensed to the public under the Creative Commons Attribution 3.0 License - NOT COMMERCIAL.</p>
+        </div>
+        <div class="col-xs-12 col-lg-4">
+            <h4>Flatfy Theme</h4>
+            <p>12 column Grid Bootstrap</p>
+            <p>Form Contact</p>
+            <p>Drag Gallery</p>
+            <p>Full Responsive</p>
+            <p>Lorem Ipsum</p>
+            <p>Go to: andreagalanti.it/flatfy</p>
+        </div>
+    </section>
+</footer>
+<script src="<?php echo get_stylesheet_directory_uri().'/static/external/jquery/dist/jquery.min.js' ; ?>"></script>
+<script src="<?php echo get_stylesheet_directory_uri().'/static/external/bootstrap/dist/js/bootstrap.min.js'; ?>"></script>
+<script src="<?php echo get_stylesheet_directory_uri().'/static/external/OwlCarousel2-2.2.1/dist/owl.carousel.min.js'; ?>"></script>
+<script>
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items:3,
+    dots:true,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+})
+
+  </script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -2,9 +2,49 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Flatfy</title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/static/external/bootstrap/dist/css/bootstrap.min.css';?>">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/static/external/font-awesome/css/font-awesome.min.css'; ?>">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/static/external/OwlCarousel2-2.2.1/dist/assets/owl.carousel.min.css'; ?>">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/static/external/OwlCarousel2-2.2.1/dist/assets/owl.theme.default.min.css'; ?>">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" >
+        <link href="https://fonts.googleapis.com/css?family=Arvo|Lato" rel="stylesheet">
     </head>
     <body>
         <header>
-
+            <div class="container-fluid conthead">
+                <div class="headerback">
+                    <h1>Flatfy</h1>
+                    <h2>Clean &amp; minimal Theme </h2>
+                    <div class="buttons">
+                        <p class="twitter"><a href="#">Twitter</a></p>
+                        <p class="download"><a href="#">Free Download</a></p>
+                    </div>
+                    <div class="circleheader">
+                        <img class="circle" src="<?php echo get_stylesheet_directory_uri() . '/static/images/circle.png';?>" alt="circle">
+                    </div>
+                </div>
+                <nav class="navbar navbar-default">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Flatfy</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="#">What is? <span class="sr-only">(current)</span></a></li>
+                            <li><a href="#">Use it</a></li>
+                            <li><a href="#">Screenshot</a></li>
+                            <li><a href="#">Credits</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </nav>
+            </div>
         </header>
