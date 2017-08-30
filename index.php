7e30a69f39b09d654dc14abfebf213faeb10e704
solene <solenantoine@gmail.com>
2017-08-30 11:06:12 +0200

    added articles 'what is?'

--- index.php
+++ index.php
@@ -1,36 +1,19 @@
 <?php get_header(); ?>
-<main>
-    <main class="row">
-        <!-- <div class="container"> -->
-        <h3><?php
-				$cat = get_the_category(); $cat = $cat[0]; echo $cat->cat_name;
-		?> </h3>
-        <p class="sdtitle">A special thanks to Death.</p>
-        <div class="firstrow">
-            <?php
-                    $args = array(
-                        'posts_per_page'   => 3,
-                        'orderby'          => 'date',
-                        'order'            => 'ASC',
-                        'category_name'    => 'what-is'
-                    );
-                    $posts = get_posts($args);
-                    get_template_part('template-parts/content_what');
-                    ?>
-           </div>
-        <div class="secondrow">
-        <?php $args = array(
-                        'posts_per_page'   => 3,
-                        'offset'           => 3,
-                        'orderby'          => 'date',
-                        'order'            => 'ASC',
-                        'category_name'    => 'what-is'
-                    );
-		        $posts = get_posts($args);
-		        get_template_part('template-parts/content_what');
-        ?>
-        </div> 
-    </main>
 
-
+<main class="row">
+	<?php
+	    get_template_part('template-parts/content_what');
+	?>
+</main>
+<aside class="firstaside">
+	<?php
+		$args = array(
+		            'posts_per_page'   => 1,
+		            'orderby'          => 'date',
+		            'category_name'    => 'use-it'
+		        );
+		$posts = get_posts($args);
+		get_template_part('template-parts/content_use');
+	?>
+</aside>
 <?php get_footer();
--- template-parts/content_what.php
+++ template-parts/content_what.php
@@ -1,7 +1,22 @@
-<?php
-foreach ($posts as $post) {
+
+    <h3><?php
+			$cat = get_the_category_by_ID(2); 
+			 echo $cat;
+	?></h3>
+	
+		<p class='sdtitle'><?php echo category_description(2); ?></p>
+
+    <div class="firstrow">
+		<?php
+			$args = array(
+				'posts_per_page'   => 3,
+				'orderby'          => 'date',
+				'order'            => 'ASC',
+				'category_name'    => 'what-is'
+			);
+			$posts = get_posts($args);
+			foreach ($posts as $post) {
     setup_postdata( $post );
-    $tags = wp_get_post_tags($post->ID);
     ?>
         <article>
             <?php the_post_thumbnail() ?>
@@ -10,5 +25,33 @@ foreach ($posts as $post) {
                 <p><?php the_content(); ?></p>
             </div>
         </article>
-
 <?php };?>
+	</div>
+    <div class="secondrow">
+		<?php
+			$args = array(
+		                'posts_per_page'   => 3,
+		                'offset'           => 3,
+		                'orderby'          => 'date',
+		                'order'            => 'ASC',
+		                'category_name'    => 'what-is'
+		            );
+		    $posts = get_posts($args);
+foreach ($posts as $post) {
+    setup_postdata( $post );
+    ?>
+        <article>
+            <?php the_post_thumbnail() ?>
+            <div class="">
+                <h4><?php the_title(); ?></h4>
+                <p><?php the_content(); ?></p>
+            </div>
+        </article>
+<?php };?>
+    </div> 
+
+
+
+
+
+
