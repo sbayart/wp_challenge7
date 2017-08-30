1e4f6f4914e2e25daabef793432cd8042660b2aa
solene <solenantoine@gmail.com>
2017-08-30 16:56:23 +0200

    'download free' added

--- footer.php
+++ footer.php
@@ -15,8 +15,21 @@
                 the_content();
             }; ?>
     </section>
-    <?php
-    		get_template_part('template-parts/content_credits'); ?>
+    <?php get_template_part('template-parts/content_credits'); ?>
+    <section class="lady">
+		<div>
+		<?php
+			$args = array(
+				'category_name'    => 'lady'
+			);
+			$posts = get_posts($args);
+			foreach ($posts as $post) {
+				setup_postdata($post); ?>
+				<h3><?php the_title(); ?></h3><?php
+                the_content();
+            }; ?>
+		</div>
+    </section>
     <?php wp_footer() ?>
 </footer>
 <script src="<?php echo get_stylesheet_directory_uri().'/static/external/jquery/dist/jquery.min.js' ; ?>"></script>
